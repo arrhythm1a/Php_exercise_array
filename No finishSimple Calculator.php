62bce96f3ab3e4cdabc7253c2356a834d2f841f7
arrhythm1a <33728772+arrhythm1a@users.noreply.github.com>
2019-12-25 15:56:12 +0100

    Update and rename Simple Calculator.php to No finishSimple Calculator.php

--- No finishSimple Calculator.php
+++ No finishSimple Calculator.php
@@ -4,7 +4,23 @@ if (@$_POST['rezultat']) {
     $prvi = $_POST['prvi_broj'];
     $drugi = $_POST["drugi_broj"];
     $operator = $_POST['operator'];
+
+    $resetporuka = ' <a href="' . htmlentities($_SERVER["REQUEST_URI"]) . '"> Resetuj</a>';
+    $prvi = trim(strip_tags($prvi));
+    $drugi = trim(strip_tags($drugi));
+
+    if (strlen($prvi) > 10 || strlen($drugi) > 10){
+        $izlaz = "<p class='error'><strong>Greska:</strong>Uneli ste broj sa 10 i vise cifara.".$resetporuka."</p>";
+    } elseif (empty($prvi) || empty($drugi)){
+        $izlaz = "<p class='error'><strong>Greska:</strong>Niste uneli brojeve".$resetporuka."</p>";;
+
+    }
+
+
 }
+
+
+
 
 
 
@@ -33,11 +49,14 @@ function Digitron($a, $b, $operator){
     }
     return $result;
 
+
 }
 
 ?>
+<form action="<?php echo htmlentities($_SERVER["REQUEST_URI"]); ?>" method="post">
+    <fieldset>
+    <legend>Digitron</legend>
 
-<form action="action.php" method="post">
     <label for="prvi_broj">Prvi broj</label>
     <input type="text" name="prvi_broj" id="prvi_broj">
 
@@ -52,7 +71,21 @@ function Digitron($a, $b, $operator){
     <label for="drugi_broj">Drugi broj</label>
     <input type="text" name="drugi_broj" id="drugi_broj">
      <input type="submit" class="button" name="rezultat" value="Rezultat">
-    <?php echo Digitron($prvi,$drugi,$operator); ?>
+    <?php
+    $resetporuka = ' <a href="' . htmlentities($_SERVER["REQUEST_URI"]) . '"> Resetuj</a>';
+    echo $resetporuka;
+    if (isset($prvi,$drugi)){
+        $izlaz = Digitron($prvi, $drugi,$operator);
+    }
+
+        if (!isset($prvi) || !isset($drugi)){
+            echo "<p class='error'>Niste uneli broj!</p>";
+        } else {
+
+            echo $izlaz;
+            }
 
 
+            ?>
+    </fieldset>
 </form>
