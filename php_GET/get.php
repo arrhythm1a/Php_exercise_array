9ed89957ad97e61c10b0789ae9b0072f57ed2c00
arrhythm1a <33728772+arrhythm1a@users.noreply.github.com>
2019-12-26 19:25:09 +0100

    Add files via upload

---
+++ php_GET/get.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+
+        <title>Php skript sa get zahtevom</title>
+    </head>
+    <body>
+        <fieldset>
+            <h1>Php primer sa GET zahtevom</h1>
+            <br><br><br>
+            <p>Poziv iste PHP stranice sa razlicitim argumentima:</p>
+            <p><a href="ispit.php?predmet=os4ip&indeks=2011/0123&ocena=10">Test primer 1:</a>OS4IP, 2011/0123, ocena 10</p>
+            <p><a href="ispit.php?predmet=os4ip&indeks=2011/0396&ocena=8">Test primer 2:</a>OS4IP, 2011/0396, ocena 8</p>
+            <p><a href="ispit.php?predmet=ir4pia&indeks=2011/0150&ocena=9">Test primer 3:</a>IR4PIA, 2011/0150, ocena 9</p>
+        </fieldset>
+    </body>
+</html>
---
+++ php_GET/ispit.php
@@ -0,0 +1,14 @@
+<table border="1">
+    <tr>
+        <td>Ispit</td>
+        <td><?php echo $_GET['predmet']; ?></td>
+    </tr>
+    <tr>
+        <td>Indeks</td>
+        <td><?php echo $_GET['indeks']; ?></td>
+    </tr>
+    <tr>
+        <td>Ocena</td>
+        <td><?php echo $_GET['ocena']; ?></td>
+    </tr>
+</table>
