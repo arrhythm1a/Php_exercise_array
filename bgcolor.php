503da8c2668cbfcb38464b8b48ddbe11933529b6
arrhythm1a <33728772+arrhythm1a@users.noreply.github.com>
2019-12-28 18:38:46 +0100

    Add files via upload

---
+++ bgcolor.php
@@ -0,0 +1,7 @@
+<?php
+$bgcolor = array("#BA55D3", "#0000FF", "#00FFFF", "#228B22", "#FFA500", "#FFFF00", "#FF0000");
+$today = date('w');
+?>
+<body bgcolor="<?php print($bgcolor[$today]); ?>">
+
+</body>
